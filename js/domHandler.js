2ab8d7fde8605285038298668ebf8a123400794c
Kristin Caron <krissycaron@gmail.com>
2017-03-27 21:41:16 -0500

    trying a different approach to printing things to the dom

--- js/domHandler.js
+++ js/domHandler.js
@@ -1,1 +1,24 @@
-CarLot.loading();
+console.log("trying to get things to print");
+
+function printToDom(){
+	var carString = "";
+	var currentCar = CarLot.getInventory();
+	for (var i=0; i<xhrData.length; i++) {
+		currentCar = xhrData.inventory[i];
+
+	  carString  += `<div class="col-sm-6 col-md-4" id="carInventoryCard">`;
+	  carString  += `<div class="thumbnail">`;
+	  carString  += `<img src="${xhrData.currentCar[i].url}">`;
+	  carString  += `<div class="caption">`;
+	  carString  += `<h3>${xhrData.currentCar[i].make}</h3>`;
+	  carString  += `<h6>${xhrData.currentCar[i].model}</h6>`;
+	  carString  += `<p>Year: ${xhrData.currentCar[i].year}</p>`;
+	  carString  += `<p>Price: ${xhrData.currentCar[i].price}</p>`;
+	  carString  += `<p>Description: ${xhrData.currentCar[i].description}</p>`;
+	  carString  += `</div></div></div>`
+	}
+
+carString.innerHTML = carContainerDiv;
+console.log(printToDom);
+};
+console.log(printToDom);
